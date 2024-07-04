18e8f6cc9e7dbbc79de4dce8c389828489d2ad93
Sudip Chowdhury <rexalex826@gmail.com>
2024-07-04 22:57:47 +0530

    some css remain and shimmer style remains

--- src/component/ProductDetail.tsx
+++ src/component/ProductDetail.tsx
@@ -5,11 +5,28 @@ import { RootState } from '../redux/store';
 import '../css/ProductDetail.css'
 
 const ProductDetail: React.FC = () => {
-  const { id } = useParams<{ id: string }>();
-  const product = useSelector((state: RootState) =>
-    state.products.items.find((item) => +(item.id) === Number(id))
-  );
-
+  // const { id } = useParams<{ id: string }>();
+  // const product = useSelector((state: RootState) =>
+  //   state.products.items.find((item) => +(item.id) === Number(id))
+  // );
+const product={avatar
+  : 
+  "https://loremflickr.com/640/480/fashion",
+  category
+  : 
+  "Computers",
+  description
+  : 
+  "Boston's most advanced compression wear technology increases muscle oxygenation, stabilizes active muscles",
+  id
+  : 
+  "1",
+  price
+  : 
+  "429.00",
+  title
+  : 
+  "Awesome Cotton Sausages"}
   if (!product) return <div>Product not found</div>;
 
   return (
@@ -17,12 +34,12 @@ const ProductDetail: React.FC = () => {
     <article className="article">
       <header className='header'>
         <h1>{product?.title}</h1>
-        
+        <hr></hr>
         <p><span className="price">₹ {product?.price}</span></p>
       </header>
-      <figure className="figure">
+      <section className="figure">
         <img  src={product?.avatar} alt={product?.title}/>
-      </figure>
+      </section>
       <section className="section">
         <h2>Product Details</h2>
         <p>Category: <span className="category">{product?.category}</span></p>
