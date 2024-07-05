d244a975b02808ab4717640d1f7567804ed0c328
Sudip Chowdhury <rexalex826@gmail.com>
2024-07-05 09:58:11 +0530

    details page's css in medium screen completed

--- src/component/ProductDetail.tsx
+++ src/component/ProductDetail.tsx
@@ -5,11 +5,28 @@ import { RootState } from '../redux/store';
 import '../css/ProductDetail.css'
 
 const ProductDetail: React.FC = () => {
-  const { id } = useParams<{ id: string }>();
-  const product = useSelector((state: RootState) =>
-    state.products.items.find((item) => +(item.id) === Number(id))
-  );
-
+  // const { id } = useParams<{ id: string }>();
+  // const product = useSelector((state: RootState) =>
+  //   state.products.items.find((item) => +(item.id) === Number(id))
+  // );
+const product={avatar
+  : 
+  "https://loremflickr.com/640/480/fashion",
+  category
+  : 
+  "Computers",
+  description
+  : 
+  "Boston's most advanced compression wear technology increases muscle oxygenation, stabilizes active muscles",
+  id
+  : 
+  "1",
+  price
+  : 
+  "429.00",
+  title
+  : 
+  "Awesome Cotton Sausages"}
   if (!product) return <div className='product_not_found'><h1>Product not found...</h1></div>;
 
   return (
@@ -24,7 +41,7 @@ const ProductDetail: React.FC = () => {
         <img  src={product?.avatar} alt={product?.title}/>
       </section>
       <section className="section">
-        <h2>Product Details</h2>
+        <h2>Product Details :</h2>
         <p>Category: <span className="category">{product?.category}</span></p>
         <p><span className="description">{product?.description}</span></p>
       </section>
